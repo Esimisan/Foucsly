8da32419309e6e84499d8700d6ef6edf4ec92678
Esimisan <folabiakra@gmail.com>
2026-07-02 03:35:17 +0100

    functionality for task creation

--- script/modal.js
+++ script/modal.js
@@ -16,6 +16,50 @@ if (event.target === modal){
 }
 };
 
+let todoTasks = [];
+
+const titleInput = document.querySelector('.js-title');
+const descriptionInput = document.querySelector('.js-description');
+const DateInput = document.querySelector('.js-date');
+const timeInput = document.querySelector('.js-time');
+const priorityInput = document.querySelector('.js-priority');
+const categoryInput = document.querySelector('.js-task-category');
+const durationInput = document.querySelector('.js-task-duration');
+const createTaskbtn = document.querySelector('.js-create-task')
+
+function addTask(){
+  const title = titleInput.value;
+  const description = descriptionInput.value;
+  const dueDate = DateInput.value;
+  const dueTime = timeInput.value;
+  const priority = priorityInput.value;
+  const category = categoryInput.value;
+  const duration = durationInput.value;
+
+  const tasks = {
+    title,
+    description,
+    dueDate,
+    dueTime,
+    priority,
+    category,
+    duration
+  };
+  todoTasks.push(tasks);
+}
+
+createTaskbtn.addEventListener('click', () => {
+  addTask();
+})
+
+
+
+
+
+
+
+
+
 openBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 window.addEventListener('click', outsideClick)
